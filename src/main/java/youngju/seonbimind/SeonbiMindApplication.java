6c9a16971f2ed29874594fa87e927a6a9e882222
ParkChanWoo0321 <op9563_@naver.com>
2026-04-11 13:18:36 +0900

    프로젝트 생성

---
+++ src/main/java/youngju/seonbimind/SeonbiMindApplication.java
@@ -0,0 +1,13 @@
+package youngju.seonbimind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonbiMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeonbiMindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/youngju/seonbimind/SeonbiMindApplicationTests.java
@@ -0,0 +1,13 @@
+package youngju.seonbimind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonbiMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
